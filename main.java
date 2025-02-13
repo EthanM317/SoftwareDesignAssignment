cb292b8924cdc984cb65ad4211822638500414d9
EthanM <ethan.medeiros1@ontariotechu.net>
2025-02-13 16:55:13 -0500

    added y or n question

--- main.java
+++ main.java
@@ -1,2 +1,20 @@
 // Example code
+import java.util.Scanner;
 
+public class main{
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = reader.next();
+
+        System.out.println("Hello " + name + "!");
+        
+        
+        System.out.print("Do you like coding? Enter y or n: ");
+        char answer = reader.next().charAt(0);
+        reader.close();
+
+        System.out.println("You entered: " + answer);
+
+    }
+}
