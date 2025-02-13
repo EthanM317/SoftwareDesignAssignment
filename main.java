efa8ac2b6bbcd581b9068dc33751a92e44e45c69
EthanM <ethan.medeiros1@ontariotechu.net>
2025-02-13 16:53:04 -0500

    added hello message

--- main.java
+++ main.java
@@ -1,2 +1,14 @@
 // Example code
+import java.util.Scanner;
 
+public class main{
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = reader.next();
+
+        System.out.println("Hello " + name + "!");
+        
+
+    }
+}
